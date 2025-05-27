b0067ec2393e9b6c38b528e7b8c5fd1b8c18f9d9
issassa <isabella.santos2@estudante.ifms.edu.br>
2025-05-27 08:11:50 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import SideBar from '$lib/components/SideBar.svelte';
+    import 'bootstrap/dist/css/bootstrap.css';
+    import 'bootstrap-icons/font/bootstrap-icons.css';
+    import * as bootstrap from 'bootstrap';
+  
+    let { children } = $props();
+  </script>
+  
+  <Header />
+  
+  <div class="container-fluid py-5 mt-1 mb-4">
+    {@render children()}
+  </div>
+  
+  <footer class="navbar bg-body-tertiary fixed-bottom">
+    <div class="container-fluid d-flex justify-content-center aling-items-center"><i class="bi bi-rocket-takeoff"></i></div>
+  </footer>
+  
+  <SideBar />
