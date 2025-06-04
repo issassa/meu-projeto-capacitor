c284c670827f2febbc5733129475ac560f34f95a
issassa <isabella.santos2@estudante.ifms.edu.br>
2025-06-04 11:11:27 -0400

    dia 04.06 -> continuação da calculadoraw

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -1,42 +1,68 @@
-    <!-- // fzr um app de calculadora  -->
-    <!-- // ctrl+espaço ativa o "autocomplete" na linha atual -->
-    <!-- // tr*5(indica que queremos 5 linhas) > td*4(indica que queremos 4 células em cada linha) => tr*5>td*4 -->
-    <!-- // tab/shift+tab para alternar entre os campos cinzas que aparecem inicialmente após a criação das linhas e células; dps de executar o código acima por exemplo -->
-    <!-- // ctrl+H substituição -->
+    <!-- 03.06
+     fzr um app de calculadora  
+     ctrl+espaço ativa o "autocomplete" na linha atual 
+     tr*5(indica que queremos 5 linhas) > td*4(indica que queremos 4 células em cada linha) => tr*5>td*4 
+     tab/shift+tab para alternar entre os campos cinzas que aparecem inicialmente após a criação das linhas e células; dps de executar o código acima por exemplo 
+     ctrl+H substituição 
+     MVP -> M (minimun) V (Value) P (Product)-->
+    <!-- 04.06
+     implemente mais operações na sua calculadora-->
+
+<script>
+    let display=$state('');
+
+    function clear() {
+        display=''; 
+    } 
+    
+    function press(c) {
+        display+=c;
+    } 
+    
+    function calc() {
+        display=eval(display);
+    } 
+</script>
+
 <div class="text-center mt-3">
-    <input class="form-control">
-<table class="table table-borderless">
+    <input class="form-control" readonly bind:value={display}/>
+<table class="table table-sm table-borderless">
     <tbody>
         <tr>
-            <td><button type="button" class="btn btn-outline-secondary w-100">C</button></td>
-            <td><button type="button" class="btn btn-light w-100">&lpar;</button></td>
-            <td><button type="button" class="btn btn-light w-100">&rpar;</button></td>
-            <td><button type="button" class="btn btn-light w-100">/</button></td>
+            <td><button type="button" class="btn bg-body-secondary w-100" onclick={()=>clear()}>C</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press('(')}>&lpar;</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(')')}>&rpar;</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press('/')}>/</button></td>
         </tr>
         <tr>
-            <td><button type="button" class="btn btn-light w-100">7</button></td>
-            <td><button type="button" class="btn btn-light w-100">8</button></td>
-            <td><button type="button" class="btn btn-light w-100">9</button></td>
-            <td><button type="button" class="btn btn-outline-secondary w-100">x</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(7)}>7</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(8)}>8</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(9)}>9</button></td>
+            <td><button type="button" class="btn bg-body-secondary w-100" onclick={()=>press('*')}>x</button></td>
+            <td><button type="button" class="btn bg-body-secondary w-100" onclick={()=>press('^')}>^</button></td>
+
         </tr>
         <tr>
-            <td><button type="button" class="btn btn-light w-100">4</button></td>
-            <td><button type="button" class="btn btn-light w-100">5</button></td>
-            <td><button type="button" class="btn btn-light w-100">6</button></td>
-            <td><button type="button" class="btn btn-outline-secondary w-100">-</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(4)}>4</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(5)}>5</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(6)}>6</button></td>
+            <td><button type="button" class="btn bg-body-secondary w-100" onclick={()=>press('-')}>-</button></td>
+            <td><button type="button" class="btn bg-body-secondary w-100" onclick={()=>press('*')}>%</button></td>
+
         </tr>
         <tr>
-            <td><button type="button" class="btn btn-light w-100">1</button></td>
-            <td><button type="button" class="btn btn-light w-100">2</button></td>
-            <td><button type="button" class="btn btn-light w-100">3</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(1)}>1</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(2)}>2</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(3)}>3</button></td>
             <td rowspan="2" style="height: 0;">
-                <button type="button" class="btn btn-outline-secondary w-100 h-100">+</button></td>
+                <button type="button" class="btn bg-body-secondary w-100 h-100" onclick={()=>press('+')}>+</button></td>
         </tr>
         <tr>
-            <td><button type="button" class="btn btn-light w-100">0</button></td>
-            <td><button type="button" class="btn btn-light w-100">.</button></td>
-            <td><button type="button" class="btn btn-light w-100">=</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press(0)}>0</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>press('.')}>.</button></td>
+            <td><button type="button" class="btn bg-danger-subtle w-100" onclick={()=>calc()}>=</button></td>
         </tr>
     </tbody>
 </table>
 </div>
+
