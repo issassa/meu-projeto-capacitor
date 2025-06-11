881042bcab7b04f1a249b70f35e63b59e2bec3f6
issassa <isabella.santos2@estudante.ifms.edu.br>
2025-06-11 11:11:23 -0400

    inicio dicionario

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -8,6 +8,7 @@
         <li class="nav-item"><a class="nav-link active" href="/todolist">Lista de Tarefas 📔</a></li>
         <li class="nav-item"><a class="nav-link active" href="/senha">Gerador de Senhas </a></li>
         <li class="nav-item"><a class="nav-link active" href="/calculadora">Calculadora 🧮</a></li>
+        <li class="nav-item"><a class="nav-link active" href="/dicionario">Dicionário 🔠</a></li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown">Dropdown</a>
---
+++ src/routes/dicionario/+page.svelte
@@ -0,0 +1,33 @@
+<!-- DESRIÇÃO DO CAP09 -->
+
+<script>
+	import { dicionario } from "$lib/dicionario";
+
+    let palavra = $state('');
+    let filtradas = $state(dicionario);
+
+    function buscar() {
+        if ((palavra == '')) {
+            filtradas = dicionario;
+            return;
+        }
+
+        filtradas = [];
+        for (const termo of dicionario) {
+            if (termo.palavra.startsWith(palavra)) {
+                filtradas.push(termo);
+            }
+        }
+    }
+</script>
+
+<div>
+    <input placeholder="digite uma palavra" oninput={buscar} bind:value={palavra}/>
+</div>
+<div> 
+   <ul>
+    {#each filtradas as termo}
+      <li><a href="/dicionario/{termo.palavra}">{termo?.palavra}</a></li>
+    {/each}
+   </ul>
+</div>
