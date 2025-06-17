f7251120d9caaa50ae3e2bd343f459d76c685c3c
issassa <isabella.santos2@estudante.ifms.edu.br>
2025-06-17 08:31:23 -0400

    dicio no processo

--- src/routes/dicionario/+page.svelte
+++ src/routes/dicionario/+page.svelte
@@ -1,31 +1,45 @@
 <script>
-	import { dicionario } from "$lib/dicionario";
+	import { dicionario } from '$lib/dicionario';
 
-    let palavra = $state('');
-    let filtradas = $state(dicionario);
+	let palavra = $state('');
+	let filtradas = $state(dicionario);
 
-    function buscar() {
-        if ((palavra == '')) {
-            filtradas = dicionario;
-            return;
-        }
+	function buscar() {
+		if (palavra == '') {
+			filtradas = dicionario;
+			return;
+		}
 
-        filtradas = [];
-        for (const termo of dicionario) {
-            if (termo.palavra.startsWith(palavra)) {
-                filtradas.push(termo);
-            }
-        }
-    }
+		filtradas = [];
+		for (const termo of dicionario) {
+			if (termo.palavra.startsWith(palavra)) {
+				filtradas.push(termo);
+			}
+		}
+	}
 </script>
 
-<div>
-    <input placeholder="digite uma palavra" oninput={buscar} bind:value={palavra}/>
-</div>
-<div> 
-   <ul>
-    {#each filtradas as termo}
-      <li><a href="/dicionario/{termo.palavra}">{termo?.palavra}</a></li>
-    {/each}
-   </ul>
-</div>
+<br />
+	<div class="card text-center mb-3" style="width: 21rem;">
+		<div class="card-body text-center">
+			<h1 class="card-title"><b>Dicionário <i class="bi bi-rocket"></i></b></h1>
+			<br />
+			<div class="input-group mb-3">
+				<input
+					class="form-control"
+					placeholder="digite uma palavra..."
+					aria-label="palavra"
+					aria-describedby="button-addon2"
+					oninput={buscar}
+					bind:value={palavra}
+				/>
+			</div>
+			<p class="card-text">aa</p>
+			<div class="text-justify"></div>
+			<ul>
+				{#each filtradas as termo}
+					<li><a href="/dicionario/{termo.palavra}">{termo?.palavra}</a></li>
+				{/each}
+			</ul>
+		</div>
+	</div>
---
+++ src/routes/dicionario/[palavra]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { page } from '$app/state';
+	import { dicionario } from '$lib/dicionario';
+
+	let termo = $state();
+
+	/** lógica que busca a variável no dicionário que é uma lista de objetos, aquele cuja propriedade "palavra" seja igual ao parâmetro de rota da palavra
+	 * ao encontar, atribua esse objeto à variável "termo"
+	 *
+	 * dps de atribuir, percorra sua definições no html usando o comando {#each}
+	 */
+
+</script>
+
+<h2>{page.params.palavra}</h2>
+
+<div class="card mb-3" style="width: 21rem;">
+	<div class="card-body text-center">
+		<h1 class="card-title"><b>Dicionário <i class="bi bi-rocket"></i></b></h1>
+		<br />
+	</div>
+	<p class="card-text">a</p>
+        <h2>{page.params.palavra}</h2>
+</div>
